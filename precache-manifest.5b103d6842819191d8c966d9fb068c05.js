59e13ca2837a8ef0f443adc09f9bc7223e36ac18
Taylor Epskamp <taylorepskamp@gmail.com>
2020-08-04 13:42:51 +0200

    Updates

---
+++ precache-manifest.5b103d6842819191d8c966d9fb068c05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f871e4bf2fe573ac2c32e2b40916af72",
+    "url": "/jamming/index.html"
+  },
+  {
+    "revision": "4749da8d81fe2ecb639b",
+    "url": "/jamming/static/css/main.490fb33c.chunk.css"
+  },
+  {
+    "revision": "7558dc6db868e84e2ba8",
+    "url": "/jamming/static/js/2.9273c18f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/jamming/static/js/2.9273c18f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4749da8d81fe2ecb639b",
+    "url": "/jamming/static/js/main.5e0e237c.chunk.js"
+  },
+  {
+    "revision": "740257ebb512bb971bb7",
+    "url": "/jamming/static/js/runtime-main.005ec7e6.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jamming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
